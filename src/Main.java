19f8d4644da93935ebc9821be53e4136e0b243e7
gk7734 <gamekiller7734@naver.com>
2023-08-21 16:05:33 +0900

    1일차

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class CMain {
-  public static void Cmain(String[] args) {
+public class Main {
+  public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("My Name is Jgh!!");
   }
